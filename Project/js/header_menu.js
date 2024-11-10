52fbc71ebf3f76a6f777ae6d2c2eb0b6e9300c0a
Ivan_Lyakhnovich <44955472+Ivan-Lyahnovich@users.noreply.github.com>
2024-11-10 17:59:32 +0500

    Delete Project directory

--- Project/js/header_menu.js
+++
@@ -1,4 +0,0 @@
-document.querySelector('.menu-icon').addEventListener('click', function () {
-    const popupMenu = document.querySelector('.popup-menu');
-    popupMenu.style.display = popupMenu.style.display === 'block' ? 'none' : 'block';
-});
